f1799d5122f3f422264b922f07613926f994ffd3
Deep Bansal <66458119+deep-bansal@users.noreply.github.com>
2020-08-10 20:56:57 +0530

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,6 +1,147 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int maxSumSubmatrix(int arr[10][10], int r, int c)
+{
+    int sum = INT_MIN;
+
+    for (int i = r - 1; i >= 0; --i)
+    {
+        for (int j = c - 2; j >= 0; --j)
+        {
+            arr[i][j] += arr[i][j + 1];
+        }
+
+    }
+
+    for (int i = r - 2; i >= 0 ; --i)
+    {
+        for (int j = c - 1; j >= 0 ; --j)
+        {
+            arr[i][j] += arr[i + 1][j];
+        }
+    }
+
+    for (int i = 0; i < r; ++i)
+    {
+        for (int j = 0; j < c; ++j)
+        {
+            sum = max(sum, arr[i][j]);
+        }
+    }
+
+    return sum;
+}
+
+
+void rotate(int arr[4][4],int r,int c)
+{
+    for (int i = 0; i < r; ++i)
+    {
+        int left = 0,right = c-1;
+        while(left <= right)
+        {
+            swap(arr[i][left],arr[i][right]);
+            left++;
+            right--;
+        }
+    }
+      for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<arr[i][j]<<",";
+        }
+        cout<<endl;
+    }
+
+    for (int i = 0; i < c; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+
+     for (int i = 0; i < r; ++i)
+    {
+        for (int j = 0; j < c; ++j)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+void sort01(int* arr,int n)
+{
+    int left = 0,right = 0;
+    while(right<= n-1)
+    {
+        if(arr[right] == 0)
+        {
+            swap(arr[left],arr[right]);
+            left++;
+            right++;
+        }
+        else 
+        {
+            right++;
+        }
+    }
+
+}
+
+void dnfAlgo(int*arr,int n)
+{
+    int left = 0,mid = 0,right = n-1;
+    while(mid <= right)
+    {
+        if(arr[mid] == 0)
+        {
+            swap(arr[mid],arr[left]);
+            mid++;
+            left++;
+        }
+        else if(arr[mid] == 1)
+        {
+            mid++;
+        }
+        else if(arr[mid] == 2)
+        {
+            swap(arr[mid],arr[right]);
+            right--;
+        }
+    }
+}
+
+int rainwater(int*arr ,int n)
+{
+    int leftMax[n],rightMax[n];
+
+    leftMax[0] = arr[0];
+
+    for (int i = 1; i < n; ++i)
+    {
+        leftMax[i] = max(arr[i],leftMax[i-1]);
+    }
+
+    rightMax[n-1] = arr[n-1];
+    for (int i = n-2; i >= 0 ; --i)
+    {
+        rightMax[i] = max(arr[i],rightMax[i+1]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        sum+= min(leftMax[i],rightMax[i]) - arr[i];
+    }
+    return sum;
+}
+
+
 
 void prefixComp(int arr[2][2],int prefixMat[2][2],int r,int c)
 {
@@ -140,6 +281,61 @@ int main(int argc, char const *argv[])
 	int r =2, c =2;
 	int sum = sumAllsubmatrix(arr, r, c);
 	cout<<sum<<endl;
+	
+	 int arr[10][10] = {{ -12, -11, -10, -9},
+                       { -4, 0, 3, 4},
+                       {1, 2, 4, 5},
+                       {2, 3, 5, 6}
+    };
+
+    int r = 4, c = 4;
+    int sum = maxSumSubmatrix(arr, r, c);
+    cout << sum << endl;
+
+   // matrix rotation
+
+    int arr[4][4] =    {{1,2,3},
+                       {5,6,7},
+                       {9,10,11},
+                       };
+
+    int r = 3, c = 3;
+
+    rotate(arr,r,c);
+
+
+    //sort 0,1
+
+    int n = 10;
+    int arr[10] = {1,0,1,0,1,1,1,0,1,0};
+    sort01(arr,n);
+    for (int i = 0; i < n; ++i)
+    {
+        cout<<arr[i]<<", ";
+    }
+    cout<<endl;
+
+
+   // sort 0,1,2
+
+    int n = 11;
+    int arr[11] = {1,2,0,1,1,0,2,0,1,2,1};
+    dnfAlgo(arr,n);
+    for (int i = 0; i < n; ++i)
+    {
+        cout<<arr[i]<<", ";
+    }
+    cout<<endl;
+
+    //rainwater harvesting
+
+    int arr[] = {0,2,1,3,0,1,2,1,2,1};
+    int n = sizeof(arr)/sizeof(int);
+
+    int total = rainwater(arr,n);
+
+    cout<<total<<endl;
+
 
 
 
